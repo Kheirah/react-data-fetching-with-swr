e2ee18ba329aeaf59b44a9b63e054c2374dd9652
Ryan Ceres <c.nizz@web.de>
2023-02-21 11:07:47 +0100

    add swr and local state for extra info

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 function useFetch(url) {
   const [data, setData] = useState();
@@ -19,8 +20,12 @@ function useFetch(url) {
 
 export default function Joke() {
   const [id, setId] = useState(0);
+  const [jokesInfo, setJokesInfo] = useState([]);
 
-  const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+  /*  const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`); */
+  const { data } = useSWR(
+    `https://example-apis.vercel.app/api/bad-jokes/${id}`
+  );
 
   function handlePrevJoke() {
     setId(data.prevId);
@@ -30,14 +35,38 @@ export default function Joke() {
     setId(data.nextId);
   }
 
+  function handleToggleFunny(id) {
+    setJokesInfo((prevJokesInfo) => {
+      const info = prevJokesInfo.find((info) => info.id === id);
+      if (info) {
+        return prevJokesInfo.map((info) =>
+          info.id === id ? { ...info, isFunny: !info.isFunny } : info
+        );
+      }
+      return [...prevJokesInfo, { id: id, isFunny: true }];
+    });
+  }
+
   if (!data) {
     return <h1>Loading...</h1>;
   }
+
+  const info = jokesInfo.find((info) => info.id === id) ?? { isFunny: false };
+  const { isFunny } = info;
 
   return (
     <>
       <small>ID: {id}</small>
       <h1>{data.joke}</h1>
+      <span
+        role="img"
+        aria-label={isFunny ? "A laughing face" : "An unamused face"}
+      >
+        {isFunny ? "🤣" : "😒"}
+      </span>
+      <button type="button" onClick={() => handleToggleFunny(id)}>
+        {isFunny ? "Stop laughing" : "Start laughing"}
+      </button>
       <div>
         <button type="button" onClick={handlePrevJoke}>
           ← Prev Joke
